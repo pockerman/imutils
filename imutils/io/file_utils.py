d28bdfcbb86c8db67feaac488f7beece42be9386
pockerman <a.giavaras@gmail.com>
2023-01-22 18:48:07 +0000

    Update tempfile module

--- imutils/io/file_utils.py
+++ imutils/io/file_utils.py
@@ -12,18 +12,6 @@ INFO = "INFO: "
 WARNING = "WARNING: "
 
 
-class TempFile:
-    def __init__(self, base_path: str = "./", ext: str = ".jpg"):
-
-        self.path = "{base_path}/{rand}{ext}".format(base_path=base_path,
-                                                     rand=str(uuid.uuid4()),
-                                                     ext=ext)
-
-    def cleanup(self):
-        os.remove(self.path)
-
-
-
 def read_json(filename: Path) -> dict:
     """Read the specified JSON file into
     a directory
--- imutils/io/tempfile.py
+++ imutils/io/tempfile.py
@@ -1,13 +1,12 @@
-# import the necessary packages
 import uuid
 import os
 
-class TempFile:
-	def __init__(self, basePath="./", ext=".jpg"):
-		# construct the file path
-		self.path = "{base_path}/{rand}{ext}".format(base_path=basePath, rand=str(uuid.uuid4()),
-			ext=ext)
 
-	def cleanup(self):
-		# remove the file
-		os.remove(self.path)
+class TempFile:
+    def __init__(self, base_path: str = "./", ext: str = ".jpg"):
+        self.path = "{base_path}/{rand}{ext}".format(base_path=base_path,
+                                                     rand=str(uuid.uuid4()),
+                                                     ext=ext)
+
+    def cleanup(self):
+        os.remove(self.path)
